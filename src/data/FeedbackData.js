44265eeb7535f7300674f01f088e684b056bd5a7
cheech-pth <michael.gogarty@gmail.com>
2022-01-09 11:30:28 +1000

    useState hook on FeedbackItem props

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,12 @@
+const FeedbackData = [
+  { id: 1, rating: 10, text: "Lorem ipsum dolar ...." },
+  { id: 2, rating: 7, text: "Compressing objects: 100%" },
+  { id: 3, rating: 2, text: "Delta compression using up to 8 threads" },
+  {
+    id: 4,
+    rating: 9,
+    text: "Writing objects: 100% (35/35), 149.82 KiB | 807.00 KiB/s, done.",
+  },
+];
+
+export default FeedbackData;
